c734cdfd121bfadaa4bfaa71dd3f19d6e08e9adc
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-05-16 12:29:18 -0300

    install headlessui - accessibility component

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,3 +1,4 @@
+import { Popover } from "@headlessui/react";
 import { ChatTeardropDots } from "phosphor-react";
 import { useState } from "react";
 export function Widget() {
@@ -9,20 +10,21 @@ export function Widget() {
 
   return (
     <>
-      <div className="absolute bottom-6 right-6">
-        {isWidgetOpen ? <p>Hello world</p> : null}
-        <button
-          onClick={toggleWidgetVisibility}
+      <Popover className="absolute bottom-6 right-6">
+        <Popover.Panel>
+          Hello world
+        </Popover.Panel>
+
+        <Popover.Button
           className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group ">
           <ChatTeardropDots className='w-6 h-6' />
-
           <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700 ease-linear'>
             <span className="pl-2">
               feadback
             </span>
           </span>
-        </button>
-      </div>
+        </Popover.Button >
+      </Popover>
     </>
   )
 }
